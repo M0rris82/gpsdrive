7361a2e6618b5fcc338cd2ea542ab38208a00fe7
tweety <tweety@e3ca9d6f-f84a-410d-b97a-e7ddc331358d>
1994-06-08 08:37:26 +0000

    added src/speech_out.h update configure and po Files
    git-svn-id: https://gpsdrive.svn.sourceforge.net/svnroot/gpsdrive/trunk@187 e3ca9d6f-f84a-410d-b97a-e7ddc331358d

---
+++ src/speech_out.h
@@ -0,0 +1,38 @@
+/***********************************************************************
+
+Copyright (c) 2001,2002 Fritz Ganter <ganter@ganter.at>
+
+Website: www.kraftvoll.at/software
+
+Disclaimer: Please do not use for navigation.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*********************************************************************/
+/*
+$Log$
+Revision 1.1  2005/04/10 20:47:49  tweety
+added src/speech_out.h
+update configure and po Files
+
+
+***********************************************************************/
+
+#ifndef GPSDRIVE_SPEECH_OUT_H
+#define GPSDRIVE_SPEECH_OUT_H
+
+
+
+#endif /* GPSDRIVE_SPEECH_OUT_H */
