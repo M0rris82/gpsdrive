0e6c4a86c69bcc113775e9a89e4cacab7c14bff6
tweety <tweety@e3ca9d6f-f84a-410d-b97a-e7ddc331358d>
1994-06-08 08:37:38 +0000

    reformatting c-sources
    git-svn-id: https://gpsdrive.svn.sourceforge.net/svnroot/gpsdrive/trunk@249 e3ca9d6f-f84a-410d-b97a-e7ddc331358d

--- src/splash.c
+++ src/splash.c
@@ -6,24 +6,27 @@ Website: www.gpsdrive.de
 
 Disclaimer: Please do not use for navigation. 
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-    *********************************************************************
+*********************************************************************
 
 
 $Log$
+Revision 1.9  2005/04/10 21:50:50  tweety
+reformatting c-sources
+
 Revision 1.8  2005/04/01 21:17:08  tweety
 Fix grid config bug. The grid was alway on after start
 reformatted reading of parameters
