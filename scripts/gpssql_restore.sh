3829abd897bb06451040185d871f4bab47e638cf
tweety <tweety@e3ca9d6f-f84a-410d-b97a-e7ddc331358d>
2005-01-14 14:13:05 +0000

    rest of the Files forgot to checkin after moving
    git-svn-id: https://gpsdrive.svn.sourceforge.net/svnroot/gpsdrive/trunk@421 e3ca9d6f-f84a-410d-b97a-e7ddc331358d

---
+++ scripts/gpssql_restore.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# This script uses a database snapshot if you ever need to restore your
+# database.
+# Usage: gpssql_restore.sh /path/to/your/backup/file
+# Example: gpssql_restore.sh ~/.gpsdrive/sqldump.02.12.20_00:30.gz
+# Copyleft 2002 by Sven Fichtner <sven.fichtner@flugfunk.de>
+
+if [ "$1" = "" ]; then
+	echo "Usage: gpssql_restore.sh /path/to/your/backup/file"
+	echo "Example: gpssql_restore.sh ~/.gpsdrive/sqldump.02.12.20_00:30.gz"
+	exit 0
+fi
+
+echo "drop database geoinfo;"| mysql -ugast -pgast
+echo "create database geoinfo;"| mysql -ugast -pgast
+gunzip < $1 | mysql -ugast -pgast geoinfo
+
