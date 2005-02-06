86be186b98e16d5dd73dbb961acc5b06d9c3cca2
tweety <tweety@e3ca9d6f-f84a-410d-b97a-e7ddc331358d>
2005-02-06 00:22:14 +0000

    poi.pl to mirror and fill POI Database with Names. This is a early Alpha version
    git-svn-id: https://gpsdrive.svn.sourceforge.net/svnroot/gpsdrive/trunk@427 e3ca9d6f-f84a-410d-b97a-e7ddc331358d

---
+++ scripts/POI/DBFuncs.pm
@@ -0,0 +1,539 @@
+package POI::DBFuncs;
+
+use strict;
+use warnings;
+
+use POSIX qw(strftime);
+use Time::Local;
+use DBI;
+use POI::Utils;
+use Data::Dumper;
+
+$|= 1;                          # Autoflush
+
+BEGIN {
+        use Exporter   ();
+        our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
+
+        # set the version for version checking
+        $VERSION     = 1.00;
+        # if using RCS/CVS, this may be preferred
+        $VERSION = sprintf "%d.%03d", q$Revision$ =~ /(\d+)/g;
+
+        @ISA         = qw(Exporter);
+        @EXPORT = qw( &type_names &type_list &db_disconnect &poi_add 
+		      &delete_all_from_source &poi_list &column_names);
+        %EXPORT_TAGS = ( );     # eg: TAG => [ qw!name1 name2! ],
+        # your exported package globals go here,
+        # as well as any optionally exported functions
+        @EXPORT_OK   = qw();
+
+}
+our @EXPORT_OK;
+
+
+END { } 
+
+# -----------------------------------------------------------------------------
+
+#retrieve Column Names for desired table
+# TODO: get it directly from DB
+sub column_names($){
+    my $table = shift;
+    my @col;
+    @col = ("address_id","lat","lon","name",
+	    "level_min","level_max","alt","comment",
+	    "last_modified",
+	    "proximity","type_id","url",
+	    "wp_id","source_id") if $table eq "poi";
+
+    @col = ("type_id","name","symbol","description") if $table eq "type";
+
+    @col = ("source_id","name","url","licence","comment","last_update") if $table eq "source";
+
+    return @col;
+}
+
+# -----------------------------------------------------------------------------
+
+sub insert_hash {
+    my ($table, $field_values) = @_;
+#    my @fields = sort keys %$field_values; # sort required
+    $field_values->{"$table.last_update"} ||= time();
+    my @fields = map { "$table.$_" } column_names($table);
+    my @values = @{$field_values}{@fields};
+    my $sql = sprintf ( "insert into %s (%s) values (%s)",
+			$table, 
+			join(',', @fields),
+			join(",", ("?") x @fields)
+			);
+    my $dbh = db_connect();
+    #print "insert_hash($table, ".Dumper(\$field_values).")\n";
+    #print "$sql\n";
+    my $sth = $dbh->prepare_cached($sql);
+    return $sth->execute(@values);
+}
+
+
+#############################################################################
+# Alle noetigen Informationen fuer den connect mit der DB
+# das sind: der host, der user und das passwort, die db ist immer die selbe
+my $dbh;
+sub db_connect() {
+  my $db           = 'geoinfo';
+  my $opt_user     = $main::db_user     || 'gast';
+  my $opt_password = $main::db_password || 'gast';
+  my $host         = 'localhost';
+
+
+  # First connect to Database
+  unless ( $dbh ) {
+      $dbh = DBI->connect(
+			  "DBI:mysql:$db:$host",
+			  $opt_user,$opt_password)
+	  || die "Can't connect: $DBI::errstr\n";
+  }
+
+  return $dbh;
+}
+
+sub db_disconnect(){
+    $dbh->disconnect()
+	if $dbh;
+}
+
+# -----------------------------------------------------------------------------
+# Delete all entries matching source with name
+sub delete_all_from_source($){
+    my $source_name = shift;
+    debug("delete_all_from_source($source_name)");
+    return unless $source_name;
+    my $source_id = source_name2id( $source_name);
+    return unless $source_id >0;
+
+    my $query = "DELETE FROM poi WHERE poi.source_id = '$source_id'";
+    my $sth=$dbh->prepare($query) or die $dbh->errstr;
+    $sth->execute() or die $sth->errstr;
+    $sth->finish;
+}
+
+# -----------------------------------------------------------------------------
+# convert source name to source_id and cache it locally
+my $source_id_cache;
+sub source_name2id($){
+    my $source_name = shift;
+    my $source_id;
+    if ( defined $source_id_cache->{$source_name} ) {
+	$source_id = $source_id_cache->{$source_name};
+    } else {
+	my $dbh = db_connect();
+	my $query = "SELECT source_id FROM source WHERE source.name = '$source_name' LIMIT 1";
+	my $sth=$dbh->prepare($query) or die $dbh->errstr;
+	$sth->execute() or die $sth->errstr;
+	my $array_ref = $sth->fetchrow_arrayref();
+	if ( $array_ref ) {
+	    $source_id = $array_ref->[0];
+	    $source_id_cache->{$source_name} = $source_id;
+	} else {
+	    # Nicht gefunden --> Neuen Eintrag anlegen
+	    $source_id=0;
+	}
+	$sth->finish;
+    }
+
+    debug("Source: $source_name -> $source_id");
+
+    return $source_id;
+}
+
+# -----------------------------------------------------------------------------
+# get a list of all type names
+sub type_names(){
+    my @type_names;
+
+    my $dbh = db_connect();
+    
+    my $query = "SELECT name FROM type";
+    
+    my $sth=$dbh->prepare($query) 
+	or die $dbh->errstr;
+    
+    $sth->execute() 
+	or die $sth->errstr;
+
+    while (my $row = $sth->fetchrow_arrayref) {
+	push(@type_names,$row->[0]);
+    }
+    $sth->finish;
+
+    return @type_names;
+}
+
+# -----------------------------------------------------------------------------
+# retrieve a complete list of known types
+sub type_list(){
+    my @type_list;
+
+    my $dbh = db_connect();
+    
+    my @columns = column_names("type");
+    my $query = "SELECT ".join(',', @columns)."  FROM type";
+    
+    my $sth=$dbh->prepare($query) 
+	or die $dbh->errstr;
+    
+    $sth->execute() 
+	or die $sth->errstr;
+
+    while (my $row = $sth->fetchrow_arrayref) {
+	my $type = {};
+	for my $i ( 0.. $#columns) {
+	    $type->{$columns[$i]} = $row->[$i];
+	}
+	push(@type_list,$type);
+    }
+    $sth->finish;
+
+    return @type_list;
+}
+
+# -----------------------------------------------------------------------------
+# retrieve first 100 entries from  poi Table
+sub poi_list(){
+    my @poi_list;
+
+    my $dbh = db_connect();
+    
+    my @columns = column_names("poi");
+    my $query = "SELECT ".join(',', @columns)."  FROM poi LIMIT 100";
+    
+    my $sth=$dbh->prepare($query) 
+	or die $dbh->errstr;
+    
+    $sth->execute() 
+	or die $sth->errstr;
+
+    while (my $row = $sth->fetchrow_arrayref) {
+	my $poi = {};
+	for my $i ( 0.. $#columns) {
+	    $poi->{$columns[$i]} = $row->[$i];
+	}
+	push(@poi_list,$poi);
+    }
+    $sth->finish;
+
+    return @poi_list;
+}
+
+#############################################################################
+# Add all Waypoints from Hash into th MySQL POI Database
+#############################################################################
+sub db_add_waypoints($){
+    my $waypoints = shift;
+    print "Adding Waypoints to Database\n";
+
+    for my $wp_name ( sort keys  %{$waypoints} ) {
+	my $values = $waypoints->{$wp_name};
+
+	unless ( defined($values->{'poi.lat'}) && 
+		 defined($values->{'poi.lon'}) ) {
+	    print "Error undefined lat/lon: ".Dumper(\$values);
+	}
+
+	correct_lat_lon($values);
+
+	for my $t (qw(Wlan Action Sqlnr Proximity) ) {
+	    unless ( defined ( $values->{$t})) {
+		$values->{$t} = 0;
+	    }
+	}
+
+	$values->{Proximity} =~ s/\s*m$//;
+	poi_add($values);
+    }
+}
+
+#############################################################################
+# Add a single poit into DB
+sub poi_add($){
+    my $poi = shift;
+    my $point = {};
+    my @columns = column_names("poi");
+    map { $point->{"poi.$_"} = ( $poi->{"poi.$_"} || $poi->{$_} || $poi->{lc($_)}) } @columns;
+    @columns = column_names("address");
+    map { $point->{"address.$_"} = ( $poi->{"address.$_"} || $poi->{$_} || $poi->{lc($_)}) } @columns;
+    @columns = column_names("source");
+    map { $point->{"source.$_"} = ( $poi->{"source.$_"} || $poi->{$_} || $poi->{lc($_)}) } @columns;
+
+    # ---------------------- SOURCE
+    #print Dumper(\$point);
+    if ( $point->{"source.name"} && ! $point->{'poi.source_id'}) {
+	my $source_id = source_name2id($point->{"source.name"});
+	# print "Source: $point->{'source.name'} -> $source_id\n";
+	
+	$point->{'source.source_id'} = $source_id;
+	$point->{'poi.source_id'}    = $source_id;
+    }
+
+    # ---------------------- ADDRESS
+    $point->{'poi.address_id'}    ||= 0;
+
+    # ---------------------- TYPE
+    $point->{'poi.type_id'}       ||= 0;
+
+    # ---------------------- POI
+    $point->{'poi.last_modified'} ||= time();
+    $point->{'poi.level_min'}         ||= 0;
+    $point->{'poi.level_max'}         ||= 0;
+    insert_hash("poi",$point);
+
+}
+
+# -----------------------------------------------------------------------------
+
+sub create_db(){
+my $create_statement='
+CREATE TABLE `address` (
+  `address_id` int(11) NOT NULL auto_increment,
+  `country` varchar(40) NOT NULL default \'\',
+  `state` varchar(80) NOT NULL default \'\',
+  `zip` varchar(5) NOT NULL default \'\',
+  `city` varchar(80) NOT NULL default \'\',
+  `city_part` varchar(80) NOT NULL default \'\',
+  `street_name` varchar(80) NOT NULL default \'\',
+  `street_number` varchar(5) NOT NULL default \'\',
+  `phone` varchar(160) NOT NULL default \'\',
+  `comment` varchar(160) NOT NULL default \'\',
+  PRIMARY KEY  (`address_id`)
+) TYPE=MyISAM;
+';
+my $dbh = db_connect();
+my $sth = $dbh->prepare($create_statement);
+$sth->execute();
+
+$create_statement='
+CREATE TABLE `navicaches` (
+  `cache_id` int(11) unsigned NOT NULL default \'0\',
+  `waypoint` varchar(7) NOT NULL default \'0\',
+  `country_code` char(2) default NULL,
+  `state` varchar(9) default NULL,
+  `city` varchar(64) default NULL,
+  `name` varchar(128) default NULL,
+  `user_name` varchar(64) default NULL,
+  `hidden_date` date default NULL,
+  `modified_datetime` datetime default NULL,
+  `latitude` float(11,8) default NULL,
+  `longitude` float(11,8) default NULL,
+  `difficulty` float(2,1) default NULL,
+  `terrain` float(2,1) default NULL,
+  `retired` varchar(5) default NULL,
+  `cache_type` varchar(16) default NULL,
+  `cache_size` varchar(16) default NULL,
+  `handicapped` varchar(5) default NULL,
+  `water` varchar(5) default NULL,
+  `restrooms` varchar(5) default NULL,
+  `parking_lot` varchar(5) default NULL,
+  `pets` varchar(5) default NULL,
+  `fees` varchar(5) default NULL,
+  `open_cache` varchar(5) default NULL,
+  `comments` text,
+  `description` text,
+  `cluetitle1` varchar(128) default NULL,
+  `clue1` text,
+  `cluetitle2` varchar(128) default NULL,
+  `clue2` text,
+  `cluetitle3` varchar(128) default NULL,
+  `clue3` text,
+  `cluetitle4` varchar(128) default NULL,
+  `clue4` text,
+  `cluetitle5` varchar(128) default NULL,
+  `clue5` text,
+  `pictitle1` varchar(128) default NULL,
+  `pic1` varchar(128) default NULL,
+  `pictitle2` varchar(128) default NULL,
+  `pic2` varchar(128) default NULL,
+  `pictitle3` varchar(128) default NULL,
+  `pic3` varchar(128) default NULL,
+  `pictitle4` varchar(128) default NULL,
+  `pic4` varchar(128) default NULL,
+  `pictitle5` varchar(128) default NULL,
+  `pic5` varchar(128) default NULL,
+  `source` varchar(64) default NULL,
+  PRIMARY KEY  (`cache_id`,`waypoint`)
+) TYPE=MyISAM;
+';
+$dbh = db_connect();
+$sth = $dbh->prepare($create_statement);
+$sth->execute();
+
+$create_statement='
+CREATE TABLE `poi` (
+  `wp_id` int(11) NOT NULL auto_increment,
+  `name` varchar(80) default NULL,
+  `type_id` int(11) NOT NULL default \'0\',
+  `lat` double default \'0\',
+  `lon` double default \'0\',
+  `alt` double default \'0\',
+  `proximity` float default \'0\',
+  `comment` varchar(255) default NULL,
+  `level_min` smallint(6) NOT NULL default \'0\',
+  `level_max` smallint(6) NOT NULL default \'0\',
+  `last_modified` date NOT NULL default \'0000-00-00\',
+  `url` varchar(160) NULL ,
+  `address_id` int(11) default \'0\',
+  `source_id` int(11) NOT NULL default \'0\',
+  PRIMARY KEY  (`wp_id`),
+  KEY `last_modified` (`last_modified`),
+  KEY `name` (`name`)
+) TYPE=MyISAM;
+';
+$dbh = db_connect();
+$sth = $dbh->prepare($create_statement);
+$sth->execute();
+
+$create_statement='
+CREATE TABLE `source` (
+  `source_id` int(11) NOT NULL auto_increment,
+  `name` varchar(80) NOT NULL default \'\',
+  `licence` varchar(160) NOT NULL default \'\',
+  `url` varchar(160) NOT NULL default \'\',
+  `comment` varchar(160) NOT NULL default \'\',
+  `last_update` date NOT NULL default \'0000-00-00\',
+  PRIMARY KEY  (`source_id`)
+) TYPE=MyISAM;
+';
+$dbh = db_connect();
+$sth = $dbh->prepare($create_statement);
+$sth->execute();
+
+$create_statement='
+CREATE TABLE `type` (
+  `type_id` int(11) NOT NULL auto_increment,
+  `name` varchar(80) NOT NULL default \'\',
+  `symbol` varchar(160) NOT NULL default \'\',
+  `description` varchar(160) NOT NULL default \'\',
+  PRIMARY KEY  (`type_id`),
+  FULLTEXT KEY `description` (`description`)
+) TYPE=MyISAM;
+';
+$dbh = db_connect();
+$sth = $dbh->prepare($create_statement);
+$sth->execute();
+
+my @icons = qw( unknown
+		Ausbildung.GrundSchule
+		Ausbildung.Gymnasium
+		Ausbildung.HauptSchule
+		Ausbildung.UNI
+		Ausbildung.VHS
+		Ausbildung.Kindergarten
+		Ausbildung.Kinderkrippe
+		Bank
+		Bank.Geldautomat
+		Bank.Geldautomat.EC
+		Transport.Blitzampel
+		Briefkasten
+		Camping Platz
+		EC-Automat
+		Einkaufszentrum
+		Essen-Lieferservice
+		Essen.Gaststätte
+		Essen.Kneipe
+		Essen.Restaurant
+		Essen.Schnellrestaurant
+		Essen.Schnellrestaurant.Burger-King
+		Essen.Schnellrestaurant.MC-Donalds
+		Feuerwehr
+		Einkaufen.Flohmarkt
+		Einkaufen.Lebensmittel.Supermarkt
+		Einkaufen.Lebensmittel
+		Einkaufen.Baumarkt
+		Transport.Flugplatz
+		Freizeit.Kino
+		Freizeit.Nationalpark
+		Freizeit.Oper
+		Freizeit.Reiterhof
+		Freizeit.Sehenswürdigkeit
+		Freizeit.Sehenswürdigkeit.Museum
+		Freizeit.Spielplatz
+		Freizeit.Sport.Fussball-Stadion
+		Freizeit.Sport.Fussballplatz
+		Freizeit.Sport.Golfplatz
+		Freizeit.Sport.Minigolfplatz
+		Freizeit.Sport.Tennisplatz
+		Freizeit.Theater
+		Friedhof
+		Fussgänger Zone
+		Geldwechsel
+		Geocache
+		Gesundheit.Ambulanz
+		Gesundheit.Apotheke
+		Gesundheit.Arzt
+		Gesundheit.Krankenhaus
+		Gesundheit.Notaufnahme
+		Gift-Shop
+		Kino
+		Kirche.Evangelisch
+		Kirche.Katholisch
+		Kirche.Synagoge
+		Messe
+		Freizeit.Modellflugplatz
+		Notruf säule
+		Transport.Park&Ride
+		Transport.Parkplatz
+		Polizei
+		Post
+		Recycling.Altglas
+		Recycling.Altpapier
+		Recycling.Wertstoffhof
+		Freizeit.Rummelplatz
+		Stadt Information
+		Stadthalle
+		Telefon-Zelle
+		Transport.Auto-Verlade-Bahnhof
+		Transport.Bahnhof
+		Transport.Bus-Haltestelle
+		Transport.Fähre
+		Transport.Güter-Verlade-Bahnhof
+		Transport.Lotsendienst
+		Transport.Mautstation
+		Transport.RaststÃ€tte
+		Transport.S-Bahn-Haltestelle
+		Transport.Strassen-Bahn-Haltestelle
+		Transport.Strassen.30-Zohne
+		Transport.Strassen.Autobahn
+		Transport.Strassen.Bundesstrasse
+		Transport.Strassen.Innerorts
+		Transport.Strassen.Landstrasse
+		Transport.Tankstelle
+		Transport.Taxi-Stand
+		Transport.U-Bahn-Haltestelle
+		Transport.Verkehr.Baustelle
+		Transport.Verkehr.Radarfalle
+		Transport.Verkehr.Stau
+		Transport.Werkstatt
+		Freizeit.Veranstaltungshalle
+		Verwaltung.Rathaus
+		Verwaltung.Zulassungsstele
+		WC
+		Transport.Auto.Werkstatt
+		Freizeit.Zoo
+		Übernachtung.Hotel
+		Übernachtung.Jugend Herberge
+		Übernachtung.Motel
+		Übernachtung.ZeltplatzSchwimmbad
+		);
+
+my $i=1;
+for my $icon  ( @icons ) {    
+    my $statement = "INSERT INTO `type` VALUES ($i,'$icon','$icon.png','$icon');\n";
+    $i++;
+    $dbh = db_connect();
+    $sth = $dbh->prepare($statement);
+    $sth->execute();
+}
+
+}
+# -----------------------------------------------------------------------------
+
+1;
---
+++ scripts/POI/Gps.pm
@@ -0,0 +1,212 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+package Gps;
+
+use IO::File;
+use File::Basename;
+use Data::Dumper;
+use Math::Trig;
+#use Date::Manip qw(ParseDate DateCalc UnixDate);
+
+require Exporter;
+use vars qw(@ISA @EXPORT @EXPORT_OK %EXPORT_TAGS $VERSION);
+
+@ISA = qw( Exporter );
+
+@EXPORT = qw( earth_distance miles2km hash2str is_in_country is_europe is_germany is_bayern is_muenchen);
+
+my $M_PI = 3.14159;
+
+my $last_point={};
+my $last_points=[];
+
+#############################################################################
+sub pow($$){
+    my $x = shift;
+    my $y = shift;
+    return ($x ** $y);
+}
+#############################################################################
+sub xx_rad2deg($){
+    my $x = shift;
+    return $x * $M_PI/180.0;
+}
+
+#############################################################################
+# Liegen die Punkte im Groben innerhalb Europas/Deutschland?
+sub is_in_country($$){
+    my $point=shift;
+    my $country=shift;
+    if ($country =~ m/europ/ ) {
+	return is_europe($point);
+    } elsif ($country =~ m/bayern/ ) {
+	return is_bayern($point);
+    } elsif ($country =~ m/muenchen/ ) {
+	return 0 if $point->{lat}  < 45;
+	return 0 if $point->{lat}  >49;
+	return 0 if $point->{lon}  <9.5;
+	return 0 if $point->{lon}  >12;
+    } else {
+	return is_germany($point);
+    }
+	return 1;
+}
+
+#############################################################################
+# Liegen die Punkte im Groben innerhalb Europas?
+sub is_europe($){
+    my $point = shift;
+    return 0 if $point->{lat}  < 5;
+    return 0 if $point->{lat}  > 60;
+    return 0 if $point->{lon}  < -10;
+    return 0 if $point->{lon}  > 20;
+#    return 0 if $point->{lat}  < 10;
+#    return 0 if $point->{lat}  > 56;
+#    return 0 if $point->{lon}  < 0;
+#    return 0 if $point->{lon}  > 16;
+    return 1;
+}
+
+#############################################################################
+# Liegen die Punkte im Groben innerhalb Deutschlands?
+sub is_germany($){
+    my $point = shift;
+    return 0 if $point->{lat}  <47;
+    return 0 if $point->{lat}  >55;
+    return 0 if $point->{lon}  <5;
+    return 0 if $point->{lon}  >15;
+    return 1;
+}
+
+#############################################################################
+# Liegen die Punkte im Groben innerhalb Bayern?
+sub is_bayern($){
+    my $point = shift;
+    return 0 if $point->{lat}  <47;
+    return 0 if $point->{lat}  >51;
+    return 0 if $point->{lon}  <5;
+    return 0 if $point->{lon}  >15;
+    return 1;
+}
+
+    
+#############################################################################
+
+sub earth_distance($$$$){
+    my $lat1 = shift || 0;
+    my $lon1 = shift || 0;
+    my $lat2 = shift || 0;
+    my $lon2 = shift || 0;
+
+    # /*
+    # my $calcedR1 = calcR(lat1);
+    # my $calcedR2 = calcR(lat2);
+
+    # my $sinradi1 = sin(rad2deg(90-lat1));
+    # my $sinradi2 = sin(rad2deg(90-lat2));
+    
+    # my $x1 = calcedR1 * cos(rad2deg(lon1)) * sinradi1;
+    # my $x2 = calcedR2 * cos(rad2deg(lon2)) * sinradi2;
+    # my $y1 = calcedR1 * sin(rad2deg(lon1)) * sinradi1;
+    # my $y2 = calcedR2 * sin(rad2deg(lon2)) * sinradi2;
+    # my $z1 = calcedR1 * cos(rad2deg(90-lat1));
+    # my $z2 = calcedR2 * cos(rad2deg(90-lat2));
+    
+    # my $calcedR = calcR((double)(lat1+lat2)) / 2;
+    # my $a = acos((x1*x2 + y1*y2 + z1*z2)/square(calcedR));
+    # */
+	
+    my $x1 = calcR( $lat1 ) * cos(rad2deg( $lon1 )) * sin(rad2deg( 90-$lat1 ));
+    my $x2 = calcR( $lat2 ) * cos(rad2deg( $lon2 )) * sin(rad2deg( 90-$lat2 ));
+    my $y1 = calcR( $lat1 ) * sin(rad2deg( $lon1 )) * sin(rad2deg( 90-$lat1 ));     
+    my $y2 = calcR( $lat2 ) * sin(rad2deg( $lon2 )) * sin(rad2deg( 90-$lat2 ));
+    my $z1 = calcR( $lat1 ) * cos(rad2deg( 90 - $lat1 ));
+    my $z2 = calcR( $lat2 ) * cos(rad2deg( 90 - $lat2 ));   
+
+    my $a = acos( ($x1*$x2 + $y1*$y2 + $z1*$z2) / pow(calcR( ($lat1+$lat2)/2),2) );
+
+    if(0){
+	if ( $a =~ m/i/ ) {
+	    my $a1 =      ($x1*$x2 + $y1*$y2 + $z1*$z2);
+	    my $a2 =                                          calcR( ($lat1+$lat2)/2) ;
+	    my $a3 =    ( ($x1*$x2 + $y1*$y2 + $z1*$z2) / pow(calcR( ($lat1+$lat2)/2),2) ) ;
+	    my $b = acos( $a3 );
+	    my $t1 = 40590335705258.5;
+	    my $t2 = 40590335705258.5;
+	    my $t3 = $t1/pow($t2,2);
+	    print "acos(1)= ".acos(1)."\n";
+	    print "acos($a3)= ".acos($a3)."\n";
+	    print "acos($t3)= ".acos($t3)."\n";
+	    print "earth_distance($lat1,$lon1,$lat2,$lon2) 	$b	1: $a1	2:$a2	3:$a3   ($x1*$x2 + $y1*$y2 + $z1*$z2)\n";
+	}
+    }
+    my $erg = calcR( ($lat1+$lat2) / 2) * $a;
+    $erg = $erg / 3340;
+    $erg =~ s/.*e-0\di\s*$/0.0/; # damit werte von  '2.98023223876953e-08i' eliminiert werden
+    return $erg;
+}
+
+# Lifted from gpsdrive 1.7
+# CalcR gets the radius of the earth at a particular latitude
+# calcxy finds the x and y positions on a 1280x1024 image of a certian scale
+#  centered on a given lat/lon.
+
+# This pulls the "real radius" of a lat, instead of a global guesstimate
+sub calcR($){
+    my $lat = shift;
+
+    my $a = 6378.137;
+    my ( $r, $sc, $x, $y, $z);
+    my $e2 = 0.081082 * 0.081082;
+    # the radius of curvature of an ellipsoidal Earth in the plane of the
+    # meridian is given by
+
+    #R' = a * (1 - e^2) / (1 - e^2 * (sin(lat))^2)^(3/2)
+
+    #where a is the equatorial radius,
+    # b is the polar radius, and
+    # e is the eccentricity of the ellipsoid = sqrt(1 - b^2/a^2)
+
+    # a = 6378 km (3963 mi) Equatorial radius (surface to center distance)
+    # b = 6356.752 km (3950 mi) Polar radius (surface to center distance)
+    # e = 0.081082 Eccentricity
+    # */
+
+    $lat = $lat * $M_PI / 180.0;
+    $sc = sin($lat);
+    $x = $a * (1.0 - $e2);
+    $z = 1.0 - $e2 * $sc * $sc;
+    $y = pow($z, 1.5);
+    $r = $x / $y;
+
+    $r = $r * 1000.0;
+    return $r;
+}
+
+my $map_width = 1280;
+my $map_height = 1024;
+my $draw_x_offset=0;
+my $draw_y_offset=0;
+
+
+sub miles2km($){
+    my $m = shift;
+    return $m * 1.6;
+}
+
+sub hash2str($){
+    my $h = shift;
+    my $erg = '';
+
+    foreach my $k ( sort keys %$h){
+	next if $k eq 'line';
+	$erg .= ", " if $erg;
+	$erg .= "$k=$h->{$k}";
+    }
+    $erg .= "	line: $h->{line} ";
+    return  $erg;
+}
+1;
