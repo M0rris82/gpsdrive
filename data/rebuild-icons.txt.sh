ea5a02fc2aabc32e5300cddae0ce9cc2184a015b
tweety <tweety@e3ca9d6f-f84a-410d-b97a-e7ddc331358d>
2006-08-31 08:01:39 +0000

    honor old icons entries do not print out scheme directory to icons file
    git-svn-id: https://gpsdrive.svn.sourceforge.net/svnroot/gpsdrive/trunk@947 e3ca9d6f-f84a-410d-b97a-e7ddc331358d

--- data/rebuild-icons.txt.sh
+++ data/rebuild-icons.txt.sh
@@ -6,10 +6,37 @@
 # Notes:
 # Run it from the data directory
 #
-cd icons
-find . -name *.png |\
-sed 's/.\///' |\
-sed 's/.png//' |\
-sed 's/\//./g' |\
-sed 's/$/\t1\t10000/' \
-> ../icons.txt
+rm -f icons.new
+for dir in classic square.big square.small ; do 
+    find icons/$dir/ -type f -name '*.png' | while read file ; do 
+	# take away icons
+	file=${file#*/}
+
+	# take waway $dir
+	file=${file#*/}
+
+	# take waway .png
+	file=${file%.png}
+
+	# take waway .png
+	file=${file//\//.}
+
+	echo ${file}
+    done
+done | sort -u | \
+while read file ; do
+    #echo
+    #echo "Testing $file"
+    file_search="^${file/\+/\\+} "
+    if grep -q -E -e "$file_search" icons.txt ; then 
+	#echo "------- take old for '$file'"
+	#grep -E -e "$file_search" icons.txt
+	#echo "------- "
+	grep -E -e "$file_search" icons.txt >>icons.new
+    else 
+	echo "------- create new entry for '$file'"
+	printf "%-40s 1\t10000\n" "${file}" >> icons.new
+    fi
+done
+diff -u -b icons.txt icons.new 
+echo "New icons list can be found in icons.new"
