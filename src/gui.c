7886d305e1c0834ed080203da6229e30457a414f
dse <dse@e3ca9d6f-f84a-410d-b97a-e7ddc331358d>
2007-01-31 22:16:02 +0000

    - added forgotten source files   (will be used for gui features soon)
    git-svn-id: https://gpsdrive.svn.sourceforge.net/svnroot/gpsdrive/trunk@1230 e3ca9d6f-f84a-410d-b97a-e7ddc331358d

---
+++ src/gui.c
@@ -0,0 +1,67 @@
+/***********************************************************************
+
+Copyright (c) 2001-2004 Fritz Ganter <ganter@ganter.at>
+
+Website: www.gpsdrive.de
+
+Disclaimer: Please do not use for navigation. 
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+***********************************************************************/
+
+/*  Include Dateien */
+#include "../config.h"
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <signal.h>
+#include <termios.h>
+#include <unistd.h>
+#include <math.h>
+#include <ctype.h>
+#include <sys/time.h>
+#include <errno.h>
+#include <dirent.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <gpsdrive_config.h>
+
+#include "gettext.h"
+
+#include "gpsdrive.h"
+#include "gui.h"
+
+extern gint do_unit_test;
+extern gint debug;
+extern GdkColor red;
+extern GdkColor black;
+extern GdkColor white;
+extern GdkColor blue;
+
+
+void gui_init()
+{
+//	GtkStyle *nightstyle = gtk_style_new();
+	
+}
