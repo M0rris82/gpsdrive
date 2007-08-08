aacadaa51b92e51aaee9aeb061ce7d2401da1f18
dse <dse@e3ca9d6f-f84a-410d-b97a-e7ddc331358d>
2007-08-08 21:49:19 +0000

    - added missing file
    git-svn-id: https://gpsdrive.svn.sourceforge.net/svnroot/gpsdrive/trunk@1522 e3ca9d6f-f84a-410d-b97a-e7ddc331358d

---
+++ src/main_gui.h
@@ -0,0 +1,48 @@
+/***********************************************************************
+
+Copyright (c) 2001-2006 Fritz Ganter <ganter@ganter.at>
+
+Website: www.gpsdrive.de
+
+Disclaimer: Please do not use for navigation. 
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    *********************************************************************
+
+***********************************************************************/
+
+#ifndef GPSDRIVE_MAIN_GUI_H
+#define GPSDRIVE_MAIN_GUI_H
+
+/* Definitions for the dashboard */
+enum
+{
+	DASH_DIST,
+	DASH_TIMEREMAIN,
+	DASH_BEARING,
+	DASH_TURN,
+	DASH_SPEED,
+	DASH_HEADING,
+	DASH_ALT,
+	DASH_TRIP,
+};
+
+gint create_main_window (gchar *geom, gint *usegeom);
+gint autobestmap_cb (GtkWidget *widget, guint datum);
+gint update_statusdisplay (void);
+gint expose_gpsfix (GtkWidget *widget, guint *datum);
+
+#endif /* GPSDRIVE_MAIN_GUI_H */
