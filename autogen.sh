f9b300c21bcfbacec6fe9ea391b2eaed7f3b646d
hamish <hamish@e3ca9d6f-f84a-410d-b97a-e7ddc331358d>
2007-08-20 05:24:25 +0000

    move config options into one place
    git-svn-id: https://gpsdrive.svn.sourceforge.net/svnroot/gpsdrive/trunk@1546 e3ca9d6f-f84a-410d-b97a-e7ddc331358d

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-echo "aclocal -I m4 --force ..." && \
-aclocal -I m4 --force && \
-echo "automake --add-missing ..." && \
-automake --add-missing && \
-echo "autoconf ..." && \
-autoconf && \
+# Script to automatically prepare config files using Automake and friends
+
+ACLOCAL_CMD="aclocal -I m4 --force"
+AUTOMAKE_CMD="automake --add-missing"
+AUTOCONF_CMD="autoconf"
+
+echo "$ACLOCAL_CMD ..." && \
+$ACLOCAL_CMD && \
+echo "$AUTOMAKE_CMD ..." && \
+$AUTOMAKE_CMD && \
+echo "$AUTOCONF_CMD ..." && \
+$AUTOCONF_CMD && \
 echo "\nFinished! Now you can start your './configure'."
