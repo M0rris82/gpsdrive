5565953a298da02a1ec996375cd7f9cef18fc116
dse <dse@e3ca9d6f-f84a-410d-b97a-e7ddc331358d>
2008-02-07 10:48:37 +0000

    - Added script 'create_geoinfo-db.pl'   This is the first step to migrate to libgda/sqlite, to get rid of the   necessity to have a mysql server installed to use all gpsdrive features.
      This script creates an sqlite database file containing a 'poi_type' table
      created from icons.xml and a 'source' table with several POI source infos.
      it does not depend on any of the other gpsdrive perl modules, and is meant
      to replace all the mysql dependant stuff soon.
      This script is for developers only; a user installation will only get the
      created file, icons.xml will not be necessary there.
      The created file completely replaces the tables poi_type and source
      currently held in mysql.
    
    
    
    git-svn-id: https://gpsdrive.svn.sourceforge.net/svnroot/gpsdrive/trunk@1875 e3ca9d6f-f84a-410d-b97a-e7ddc331358d

---
+++ scripts/create_geoinfo-db.pl
@@ -0,0 +1,266 @@
+#!/usr/bin/perl
+# Create SQLite Database file used by GpsDrive
+# Fill it with:
+#   - POI Sources predefined in this script
+#   - POI Types as defined in icons.xml
+#   TODO: - Field Types for additional POI Information (poi_extra)
+
+########################################################################################
+
+my $default_lang  = 'de';
+my $default_licence = 'Creative Commons Attribution-ShareAlike 2.0';
+
+########################################################################################
+
+# Get version number from version-control system, as integer 
+my $version = '$Revision: 1824 $';
+$Version =~ s/\$Revision:\s*(\d+)\s*\$/$1/;
+ 
+my $VERSION ="create_geoinfo-db.pl (c) Guenther Meyer
+Version 0.1-$Version";
+
+use strict;
+use warnings;
+
+use DBI;
+use File::Copy;
+use XML::Twig;
+use Getopt::Long;
+
+my $lang;
+my $db_file;
+my $dbh;
+
+
+########################################################################################
+# Execute SQL statement
+#
+sub db_exec($){
+    my $statement = shift;
+
+    my $sth = $dbh->prepare($statement);
+    unless ( $sth->execute() ) {
+        warn "Error in query '$statement'\n";
+        $sth->errstr;
+        return 0;
+    }
+    return 1;
+}
+
+
+########################################################################################
+# Creata db file and tables
+#
+sub create_dbfile(){
+    my $create_statement;
+    my $sth;
+
+    move("$db_file","$db_file.old");
+
+    $create_statement="CREATE DATABASE geoinfo;";
+    $dbh = DBI->connect("dbi:SQLite:dbname=$db_file",'','');
+    $dbh->{unicode} = 1;
+
+    # ------- POI_TYPE
+    db_exec('CREATE TABLE poi_type (
+		poi_type       VARCHAR(160)  PRIMARY KEY,
+		scale_min      INTEGER       NOT NULL default \'1\',
+		scale_max      INTEGER       NOT NULL default \'25000\',
+		title          VARCHAR(160)  NULL default \'\',
+		description    VARCHAR(160)  NULL default \'\');') or die;
+
+     # ------- SOURCE
+    db_exec('CREATE TABLE source (
+		source_id      INTEGER      PRIMARY KEY,
+		name           VARCHAR(80)  NOT NULL default \'\',
+		comment        VARCHAR(160) NOT NULL default \'\',
+		last_update    DATE         NOT NULL default \'0000-00-00\',
+		url            VARCHAR(160) NOT NULL default \'\',
+		licence        VARCHAR(160) NOT NULL default \'\');') or die;
+ 
+}
+
+
+########################################################################################
+# Fill poi_type table
+#
+sub fill_default_poi_types {
+    my $i=1;
+    my $used_icons ={};
+
+    my $unused_icon ={};
+    my $existing_icon ={};
+
+    my $icon_file='../data/map-icons/icons.xml';
+    $icon_file = '../share/map-icons/icons.xml'         unless -s $icon_file;
+    $icon_file = '/usr/local/share/icons/map-icons/icons.xml' unless -s $icon_file;
+    $icon_file = '/usr/local/share/map-icons/icons.xml' unless -s $icon_file;
+    $icon_file = '/usr/share/icons/map-icons/icons.xml' unless -s $icon_file;
+    $icon_file = '/usr/share/map-icons/icons.xml'       unless -s $icon_file;
+    $icon_file = '/opt/gpsdrive/icons.xml'              unless -s $icon_file;
+    die "no Icon File found" unless -s $icon_file;
+
+    our $title = ''; our $title_en = '';
+    our $description = ''; our $description_en = '';
+
+    # parse icon file
+    #
+    my $twig= new XML::Twig
+    (
+       TwigHandlers => { rule        => \&sub_poi,
+                         title       => \&sub_title,
+                         description => \&sub_desc }
+    );
+    $twig->parsefile( "$icon_file");
+    my $rules= $twig->root;
+
+    $twig->purge;
+
+    sub sub_poi
+    {
+      my ($twig, $poi_elm) = @_;
+      if ($poi_elm->first_child('condition')->att('k') eq 'poi')
+      {
+        my $name = $poi_elm->first_child('geoinfo')->first_child('name')->text;
+        my $scale_min = $poi_elm->first_child('scale_min')->text;
+        my $scale_max = $poi_elm->first_child('scale_max')->text;
+        $title = $title_en unless ($title);
+	$description = $description_en unless ($description);
+
+	# replace ' by something else, because otherwise the sql statement wil fail
+	#$description =~ s/'/&#0039;/g;
+	$description =~ s/'/&apos;/g;
+
+	print "Adding POI: $name\n";
+	print "            $title - $description\n";
+
+	db_exec(
+	  "INSERT INTO `poi_type` ".
+          "(poi_type, scale_min, scale_max, title, description) ".
+	  "VALUES ('$name','$scale_min','$scale_max','$title','$description');") 
+	    or die;
+      }
+      $title = ''; $title_en = '';
+      $description = ''; $description_en = '';
+    }
+
+    sub sub_title
+    {
+      my ($twig, $title_elm) = @_;
+      if ($title_elm->att('lang') eq 'en')
+        { $title_en = $title_elm->text; }
+      elsif ($title_elm->att('lang') eq $lang)
+        { $title = $title_elm->text; }
+    }
+
+    sub sub_desc
+    {
+      my ($twig, $desc_elm) = @_;
+      if ($desc_elm->att('lang') eq 'en')
+        { $description_en = $desc_elm->text; }
+      elsif ($desc_elm->att('lang') eq $lang)
+        { $description = $desc_elm->text; }
+    }
+}
+
+
+########################################################################################
+# Fill source table
+#
+sub fill_default_sources() {   # Just some Default Sources
+
+    my @sources = (
+      { source_id   => '1',
+        name        => 'unknown',
+        comment     => 'Unknown source or source not defined', 
+        last_update => '2007-01-03',
+        url         => 'http://www.gpsdrive.cc/',
+        licence     => 'unknown'
+      },
+      { source_id   => '2',
+        name        => 'way.txt',
+        comment     => 'Data imported from way.txt', 
+        last_update => '2007-01-03',
+        url         => 'http://www.gpsdrive.cc/',
+        licence     => 'unknown'
+      },
+      { source_id   => '3',
+        name        => 'user',
+	comment     => 'Data entered by the GPSDrive-User',
+	last_update => '2007-01-23',
+	url         => 'http://www.gpsdrive.cc/',
+	licence     => $default_licence
+      },
+      { source_id   => '4',
+        name        => 'OpenStreetMap.org',
+        comment     => 'General Data imported from the OpenStreetMap Project', 
+        last_update => '2007-01-03',
+        url         => 'http://www.openstreetmap.org/',
+        licence     => 'Creative Commons Attribution-ShareAlike 2.0'
+      },
+      { source_id   => '5',
+        name        => 'groundspeak',
+        comment     => 'Geocache data from Groundspeak', 
+        last_update => '2007-01-30',
+        url         => 'http://www.groundspeak.com/',
+        licence     => 'unknown'
+      },
+      { source_id   => '6',
+        name        => 'opencaching',
+        comment     => 'Geocache data from Opencaching', 
+        last_update => '2007-09-30',
+        url         => 'http://www.opencaching.de/',
+        licence     => 'unknown'
+      },
+      
+      { source_id   => '7',
+        name        => 'friendsd',
+        comment     => 'Position received from friendsd server', 
+        last_update => '2007-09-30',
+        url         => 'http://friendsd.gpsdrive.de/',
+        licence     => 'none'
+      },
+      { source_id   => '8',
+        name        => 'fon',
+        comment     => 'Access point data from FON', 
+        last_update => '2007-09-30',
+        url         => 'http://www.fon.com/',
+        licence     => 'unknown'
+      },
+    );
+
+    foreach (@sources) {
+      print "Adding Source: $$_{'name'} - $$_{'url'}\n";
+      db_exec(
+        "INSERT INTO `source` ".
+          "(source_id, name, comment, last_update, url, licence) ".
+	  "VALUES ($$_{'source_id'},'$$_{'name'}','$$_{'comment'}',".
+	  "'$$_{'last_update'}','$$_{'url'}','$$_{'licence'}');") or die;
+    }
+
+}
+
+
+########################################################################################
+#
+#                     Main
+#
+########################################################################################
+
+# Set defaults and get options from command line
+Getopt::Long::Configure('no_ignore_case');
+GetOptions (
+	     'lang=s'              => \$lang,
+	   );
+
+$lang = $lang || $default_lang;
+$db_file = "./geoinfo.$lang.db"; 
+
+
+print "$VERSION\n";
+
+create_dbfile();
+fill_default_sources();
+fill_default_poi_types();
+
+
