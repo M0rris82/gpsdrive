f4de628a63266527a081b131805bd137c07f62ec
dse <dse@e3ca9d6f-f84a-410d-b97a-e7ddc331358d>
2008-04-14 21:43:00 +0000

    - added table for osm-matching to geoinfo database
    git-svn-id: https://gpsdrive.svn.sourceforge.net/svnroot/gpsdrive/trunk@1916 e3ca9d6f-f84a-410d-b97a-e7ddc331358d

--- scripts/create_geoinfo-db.pl
+++ scripts/create_geoinfo-db.pl
@@ -17,7 +17,12 @@ my $version = '$Revision: 1824 $';
 $Version =~ s/\$Revision:\s*(\d+)\s*\$/$1/;
  
 my $VERSION ="create_geoinfo-db.pl (c) Guenther Meyer
-Version 0.1-$Version";
+Version 0.2-$Version";
+
+# History:
+#
+# v0.2 Added Table for OpenStreetMap-Matching
+# v0.1 Initial Release
 
 use strict;
 use warnings;
@@ -77,7 +82,12 @@ sub create_dbfile(){
 		last_update    DATE         NOT NULL default \'0000-00-00\',
 		url            VARCHAR(160) NOT NULL default \'\',
 		licence        VARCHAR(160) NOT NULL default \'\');') or die;
- 
+
+     # ------- OPENSTREETMAP
+    db_exec('CREATE TABLE osm (
+		key            VARCHAR(80)  NOT NULL default \'\',
+		value          VARCHAR(160) NOT NULL default \'\',
+		poi_type       VARCHAR(160) NOT NULL default \'unknown\');') or die;
 }
 
 
@@ -256,6 +266,202 @@ sub fill_default_sources() {   # Just some Default Sources
 
 
 ########################################################################################
+# Fill openstreetmap table
+#
+sub fill_osm_table() {
+
+my %osm_table =
+(
+ 'aeroway' =>
+ {
+   'airport'		=>	'transport.airport',
+   'helipad'		=>	'transport.airport.helipad',
+   'terminal'		=>	'transport.airport.terminal',
+ },
+ 'amenity' =>
+ {
+   'arts_centre'	=>	'public.arts_centre',
+   'atm'		=>	'money.atm',
+   'bicycle_parking'	=>	'vehicle.parking.bike',
+   'bank'		=>	'money.bank',
+   'bank;atm'		=>	'money.bank',
+   'bus_stop'		=>	'transport.bus',
+   'bus_station'	=>	'transport.bus',
+   'courthouse'		=>	'public.administration.court_of_law',
+   'church'		=>	'religion.church',
+   'cafe'		=>	'food.cafe',
+   'cemetery'		=>	'religion.cemetery',
+   'cinema'		=>	'recreation.cinema',
+   'clinic'		=>	'health.hospital',
+   'community_centre'	=>	'public',
+   'college'		=>	'education.college',
+   'doctor'		=>	'health.doctor',
+   'doctors'		=>	'health.doctor',
+   'embassy'		=>	'public.administration.embassy',
+   'fast_food'		=>	'food.fastfood',
+   'fire_station'	=>	'public.firebrigade',
+   'fuel'		=>	'vehicle.fuel_station',
+   'garage'		=>	'vehicle.repair_shop',
+   'grave_yard'		=>	'religion.cemetery',
+   'hospital'		=>	'health.hospital',
+   'kindergarden'	=>	'education.kindergarden',
+   'library'		=>	'shopping.rental.library',
+   'museum'		=>	'sightseeing.museum',
+   'park'		=>	'recreation.park',
+   'parking'		=>	'vehicle.parking',
+   'pharmacy'		=>	'health.pharmacy',
+   'phone_box'		=>	'public.telephone',
+   'place_of_worship'	=>	'religion.church',
+   'playground'		=>	'recreation.playground',
+   'police'		=>	'public.police',
+   'post_box'		=>	'public.post_box',
+   'post_office'	=>	'public.post_office',
+   'postbox'		=>	'public.post_box',
+   'prison'		=>	'public.administration.prison',
+   'pub'		=>	'food.pub',
+   'public_building'	=>	'public',
+   'recycling'		=>	'public.recycling',
+   'restaurant'		=>	'food.restaurant',
+   'school'		=>	'education.school',
+   'shop'		=>	'shopping',
+   'shops'		=>	'shopping',
+   'supermarket'	=>	'shopping.supermarket',
+   'telephone'		=>	'public.telephone',
+   'taxi'		=>	'transport.taxi',
+   'theatre'		=>	'recreation.theater',
+   'toilets'		=>	'public.toilets',
+   'townhall'		=>	'public.administration.townhall',
+   'university'		=>	'education.university',
+   'village_hall'	=>	'public.administration.townhall',
+ },
+ 'highway' =>
+ {
+   'bus_stop'	=>	'transport.bus',
+   'toll_booth'	=>	'vehicle.toll_station',
+ },
+ 'landuse' =>
+ {
+   'cave'	=>	'misc.landmark.cave',
+   'peak'	=>	'misc.landmark.peak',
+   'spring'	=>	'misc.landmark.spring',
+ },
+ 'leisure' =>
+ {
+   'common'		=>	'recreation',
+   'garden'		=>	'recreation.park',
+   'golf_course'	=>	'sports.golf',
+   'marina'		=>	'nautical.marina',
+   'nature_reserve'	=>	'recreation.nature_reserve',
+   'park'		=>	'recreation.park',
+   'pitch'		=>	'sports.pitch',
+   'playground'		=>	'recreation.playground',
+   'playing_fields'	=>	'sports.pitch',
+   'sports_centre'	=>	'sports.sports_centre',
+   'sport'		=>	'sports',
+   'stadium'		=>	'sports.stadium',
+   'swimming_pool'	=>	'sports.swimming',
+   'water_park'		=>	'recreation.water_park',
+   'zoo'		=>	'recreation.zoo',
+ },
+ 'man_made' =>
+ {
+   'barn'		=>	'misc.landmark.barn',
+   'cairn'		=>	'misc.landmark.cairn',
+   'gasometer'		=>	'misc.landmark.gasometer',
+   'lighthouse'		=>	'misc.landmark.lighthouse',
+   'power_fossil'	=>	'misc.landmark.power.fossil',
+   'power_hydro'	=>	'misc.landmark.power.hydro',
+   'power_wind'		=>	'misc.landmark.power.wind',
+   'wastewater_plant'	=>	'misc.landmark.wastewater_plant',
+   'water_tower'	=>	'misc.landmark.water_tower',
+   'windmill'		=>	'misc.landmark.windmill',
+ },
+ 'place' =>
+ {
+   'city'	=>	'places.settlement.city',
+   'hamlet'	=>	'places.settlement.hamlet',
+   'suburb'	=>	'places.settlement.suburb',
+   'town'	=>	'places.settlement.town',
+   'village'	=>	'places.settlement.village',
+ },
+ 'railway' =>
+ {
+   'halt'	=>	'transport.railway_small',
+   'station'	=>	'transport.railway',
+   'tram_stop'	=>	'transport.tram',
+ },
+ 'sport' =>
+ {
+   'basketball'	=>	'sports.basketball',
+   'baseball'	=>	'sports.baseball',
+   'bowling'	=>	'sports.bowling',
+   'bowls'	=>	'sports.bowling',
+   'climbing'	=>	'sports.climbing',
+   'cricket'	=>	'sports.cricket',
+   'cycling'	=>	'sports.cycling',
+   'equestrian'	=>	'sports.riding',
+   'football'	=>	'sports.football',
+   'golf'	=>	'sports.golf',
+   'golf_course'	=>	'sports.golf',
+   'gymnastics'	=>	'sports.gymnastics',
+   'hockey'	=>	'sports.hockey',
+   'motor'	=>	'sports.motor',
+   'multi'	=>	'sports.centre',
+   'pitch'	=>	'sports.pitch',
+   'polo'	=>	'sports.polo',
+   'rugby'	=>	'sports.rugby',
+   'sailing'	=>	'sports.sailing',
+   'skateboard'	=>	'sports.skate_park',
+   'skating'	=>	'sports.skating',
+   'skiing'	=>	'sports.skiing',
+   'soccer'	=>	'sports.soccer',
+   'sports_centre'	=>	'sports.centre',
+   'squash'	=>	'sports.squash',
+   'swimming'	=>	'sports.swimming',
+   'table_tennis'	=>	'sports.table_tennis',
+   'tennis'	=>	'sports.tennis',
+   'track'	=>	'sports.track',
+   'volleyball'	=>	'sports.volleyball',
+ },
+ 'tourism' =>
+ {
+   'attraction'		=>	'sightseeing',
+   'camp_site'		=>	'accommodation.camping',
+   'caravan_site'	=>	'accommodation.camping.caravan',
+   'guest_house'	=>	'accommodation.guest_house',
+   'hostel'		=>	'accommodation.hostel',
+   'hotel'		=>	'accommodation.hotel',
+   'information'	=>	'misc.information',
+   'museum'		=>	'sightseeing.museum',
+   'motel'		=>	'accommodation.motel',
+   'picnic_site'	=>	'recreation.picnic',
+   'theme_park'		=>	'recreation.theme_park',
+   'viewpoint'		=>	'sightseeing.viewpoint',
+   'zoo'		=>	'recreation.zoo',
+ },
+# 'name' =>
+# {
+#   'foo' => 'bar',
+# }
+);
+
+  my $key;
+  my $value;
+
+  foreach $key (keys %osm_table)
+  {
+    for $value (keys %{$osm_table{$key}})
+    {
+        db_exec(
+        "INSERT INTO `osm` (key,value,poi_type) VALUES ('$key','$value','$osm_table{$key}{$value}');")
+          or die;
+    }
+  }
+
+}
+
+
+########################################################################################
 #
 #                     Main
 #
@@ -279,5 +485,5 @@ print "$VERSION\n";
 create_dbfile();
 fill_default_sources();
 fill_default_poi_types();
-
+fill_osm_table();
 
