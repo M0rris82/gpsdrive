cdf490cb7ab23ac8902980e5826d93d783cf049c
hbowman <hbowman@e3ca9d6f-f84a-410d-b97a-e7ddc331358d>
2008-09-28 08:45:32 +0000

    minor script cleanup
    git-svn-id: https://gpsdrive.svn.sourceforge.net/svnroot/gpsdrive/trunk@2085 e3ca9d6f-f84a-410d-b97a-e7ddc331358d

--- scripts/gpsd_nmea.sh
+++ scripts/gpsd_nmea.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 if echo "$@" | grep -q -e '--help' ; then
-    echo "This script connects to the gpsd and switches your GPS to using nmea protocol"
+    echo "This script connects to Gpsd and switches your GPS to use the"
+    echo "  NMEA protocol. Sometimes the GPS can get stuck using, e.g., the"
+    echo "  SiRF binary protocol. Note this may not be needed, as modern"
+    echo "  versions of Gpsd output pseudo-NMEA sentences when the client"
+    echo "  requests 'R' raw-mode."
     exit -1
 fi
 
-echo "Switching the local gpsd to NMEA Mode"
+echo "Switching the GPS connected to the local Gpsd into NMEA Mode ..."
 (echo "N=0"; sleep 1 )  | telnet localhost 2947
