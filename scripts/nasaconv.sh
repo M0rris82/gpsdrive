894039040c07b60e502dcd9e19b6505c3e382fc0
hbowman <hbowman@e3ca9d6f-f84a-410d-b97a-e7ddc331358d>
2008-09-28 08:39:33 +0000

    minor script cleanup
    git-svn-id: https://gpsdrive.svn.sourceforge.net/svnroot/gpsdrive/trunk@2084 e3ca9d6f-f84a-410d-b97a-e7ddc331358d

--- scripts/nasaconv.sh
+++ scripts/nasaconv.sh
@@ -1,13 +1,31 @@
 #!/bin/sh
 
 if echo "$@" | grep -q -e '--help' ; then
-    echo "This is an example script to convert Nasa Maps"
+    echo "This is an example script to convert raw Nasa Maps into GeoTIFFs"
+    echo "See the README.nasamaps file that comes with the GpsDrive documentation"
     exit -1
 fi
 
 
+if [ -e land_shallow_topo_8192.tif ] ; then
+   gdal_translate -a_srs EPSG:4326 -a_ullr -180 90 180 -90 -of GTiff -co "TILED=YES" \
+      -co "COMPRESS=DEFLATE" land_shallow_topo_8192.tif land_shallow_topo_8192.gtiff
+fi
 
-gdal_translate -a_srs EPSG:4326 -a_ullr -180 90 180 -90 -of GTiff -co "TILED=YES" -co "COMPRESS=DEFLATE" land_shallow_topo_8192.tif land_shallow_topo_8192.gtiff
-gdal_translate -a_srs EPSG:4326 -a_ullr -180 90 180 -90 -of GTiff -co "TILED=YES" -co "COMPRESS=DEFLATE" land_shallow_topo_21600.tif land_shallow_topo_21600.gtiff
-gdal_translate -a_srs EPSG:4326 -a_ullr -180 90 0 -90 -of GTiff -co "TILED=YES" -co "COMPRESS=DEFLATE" land_shallow_topo_west.tif land_shallow_topo_west.gtiff
-gdal_translate -a_srs EPSG:4326 -a_ullr 0 90 180 -90 -of GTiff -co "TILED=YES" -co "COMPRESS=DEFLATE" land_shallow_topo_east.tif land_shallow_topo_east.gtiff
+
+if [ -e land_shallow_topo_21600.tif ] ; then
+   gdal_translate -a_srs EPSG:4326 -a_ullr -180 90 180 -90 -of GTiff -co "TILED=YES" \
+      -co "COMPRESS=DEFLATE" land_shallow_topo_21600.tif land_shallow_topo_21600.gtiff
+fi
+
+
+if [ -e land_shallow_topo_west.tif ] ; then
+   gdal_translate -a_srs EPSG:4326 -a_ullr -180 90 0 -90 -of GTiff -co "TILED=YES" \
+      -co "COMPRESS=DEFLATE" land_shallow_topo_west.tif land_shallow_topo_west.gtiff
+fi
+
+
+if [ -e land_shallow_topo_east.tif ] ; then
+   gdal_translate -a_srs EPSG:4326 -a_ullr 0 90 180 -90 -of GTiff -co "TILED=YES" \
+      -co "COMPRESS=DEFLATE" land_shallow_topo_east.tif land_shallow_topo_east.gtiff
+fi
