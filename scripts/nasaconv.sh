c967bb57504e57fcc4d57c7200c9560256c3463c
hbowman <hbowman@e3ca9d6f-f84a-410d-b97a-e7ddc331358d>
2008-09-30 06:52:13 +0000

    describe easier method
    git-svn-id: https://gpsdrive.svn.sourceforge.net/svnroot/gpsdrive/trunk@2090 e3ca9d6f-f84a-410d-b97a-e7ddc331358d

--- scripts/nasaconv.sh
+++ scripts/nasaconv.sh
@@ -6,6 +6,29 @@ if echo "$@" | grep -q -e '--help' ; then
     exit -1
 fi
 
+# Better:
+#  just create a "world file" of the same name as the .tif but with a .wld extension
+#
+# The 0.0083333 is cell resolution (in degrees, 0.5'/60) and N,S,E,W
+#  boundaries are reduced 1/2 a cell as these refer to cell centers of
+#  the outer rows, not the outer boundaries of the cell.
+#
+#     land_shallow_topo_east.wld 
+# 0.008333333333333
+# 0.000000000000000
+# 0.000000000000000
+# -0.008333333333333
+# 0.00416666666666665
+# 89.99583333333334
+# 
+#     land_shallow_topo_west.wld 
+# 0.008333333333333
+# 0.000000000000000
+# 0.000000000000000
+# -0.008333333333333
+# -179.9958333333333
+# 89.99583333333334
+# 
 
 if [ -e land_shallow_topo_8192.tif ] ; then
    gdal_translate -a_srs EPSG:4326 -a_ullr -180 90 180 -90 -of GTiff -co "TILED=YES" \
