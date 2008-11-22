d8006e715d64772429790dcc666594fcfa09f7b4
hbowman <hbowman@e3ca9d6f-f84a-410d-b97a-e7ddc331358d>
2008-11-22 04:48:33 +0000

    add script used to generate .txt and .html versions of man page
    git-svn-id: https://gpsdrive.svn.sourceforge.net/svnroot/gpsdrive/trunk@2154 e3ca9d6f-f84a-410d-b97a-e7ddc331358d

---
+++ scripts/gen_manpage.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#### fetch man page and convert to HTML
+
+cd working
+
+# checkout
+svn export http://gpsdrive.svn.sourceforge.net/svnroot/gpsdrive/trunk/man/gpsdrive.1
+
+# convert to text file
+man ./gpsdrive.1 | col -b | grep -v '^XXX' > gpsdrive_manual-en.txt
+
+# convert to HTML
+man2html gpsdrive.1 | \
+  sed -e 's+^Content-type: text/html$+<!-- Content-type: text/html -->+' \
+      -e 's+^Section: User Commands.*<BR>\(.*\)+<B>Jump to \1</B>+' \
+      -e 's+<A HREF=".*/cgi-bin/man/man2html">Return to Main Contents</A>+\n+' \
+      -e 's+<A HREF=".*/cgi-bin/man/man2html">man2html</A>+man2html+' \
+      -e 's+<BODY>+<BODY bgcolor="#F0F0F0">+' \
+  > gpsdrive_manual-en.html
+
+# copy to webspace
+#cp gpsdrive_manual-en.[th]* /home/groups/g/gp/gpsdrive/htdocs/
