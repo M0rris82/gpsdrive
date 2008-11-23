4347c022e8590a90a69490441038be88f76d8c2a
hbowman <hbowman@e3ca9d6f-f84a-410d-b97a-e7ddc331358d>
2008-11-23 23:49:51 +0000

    emphasize that GDAL support is experimental
    git-svn-id: https://gpsdrive.svn.sourceforge.net/svnroot/gpsdrive/trunk@2161 e3ca9d6f-f84a-410d-b97a-e7ddc331358d

--- DefineOptions.cmake
+++ DefineOptions.cmake
@@ -1,5 +1,5 @@
 option(WITH_SCRIPTS "Install perl scripts and modules" ON)
 option(WITH_MAPNIK "Build with mapnik support" ON)
 option(WITH_GDA3 "Build with GDA3 database support" ON)
-option(WITH_GDAL "Build with GDAL support" OFF)
+option(WITH_GDAL "Build with experimental GDAL support" OFF)
 option(WITH_SPEECH "Build with support of speech output" ON)
