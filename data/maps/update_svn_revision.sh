55ddaad77ebb5796a99211c084d50d2ddb08a233
ostertag <ostertag@e3ca9d6f-f84a-410d-b97a-e7ddc331358d>
2009-01-22 22:28:43 +0000

    New method to gather svnrevision
    git-svn-id: https://gpsdrive.svn.sourceforge.net/svnroot/gpsdrive/trunk@2288 e3ca9d6f-f84a-410d-b97a-e7ddc331358d

--- data/maps/update_svn_revision.sh
+++ data/maps/update_svn_revision.sh
@@ -1,4 +1,8 @@
-svnrevision=`svnversion`
+#!/bin/bash
+# replace the revision number in debian/changelog to the last time this 
+# Subtree was modified
+
+svnrevision=`svn info . | grep "Last Changed Rev" | sed 's/Last Changed Rev: //'`
 svnrevision=${svnrevision/M/}
 
 if [ -n "svnrevision" ] ; then
