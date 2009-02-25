3a2d312db5ee19e887e68f224b6717ba3e1bb0a9
ostertag <ostertag@e3ca9d6f-f84a-410d-b97a-e7ddc331358d>
2009-02-25 05:13:11 +0000

    Add section to create python Manpages
    git-svn-id: https://gpsdrive.svn.sourceforge.net/svnroot/gpsdrive/trunk@2303 e3ca9d6f-f84a-410d-b97a-e7ddc331358d

--- debian/create_misssing_man_pages.sh
+++ debian/create_misssing_man_pages.sh
@@ -61,4 +61,30 @@ find $src_dir -name "*.sh" | grep -v -e '\#' -e '~' |\
     fi
 done
 
+# Python Binaries
+find $src_dir -name "*.py" | grep -v -e '\#' -e '~' |\
+    while read src_fn ; do 
+    filename="`basename $src_fn`"
+    filename=${filename%.py}
+
+    # extract manpage from python program
+    man1_fn="$man1_path/${filename}.1"
+    if pydoc "$src_fn" >/dev/null 2>&1 ; then
+	echo "Create Man Page '$man1_fn' from python-file '$src_fn'"
+	pydoc $src_fn >"$man1_fn"
+    else
+	if grep -q -e "--man" "$src_fn"; then
+	    echo "Create Man Page '$man1_fn' with '$src_fn'  --man"
+	    python $src_fn --man >"$man1_fn"
+	else
+	    if grep -q -e "--help" "$src_fn"; then
+		echo "Create Man Page '$man1_fn' with '$src_fn'  --help"
+		python $src_fn --help >"$man1_fn"
+	    else
+		echo "No idea how to create Man Page for $src_fn"
+	    fi
+	fi
+    fi
+done
+
 echo "Finished Creating Man Pages."
