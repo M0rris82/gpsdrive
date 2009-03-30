72d2702532f42293b3d9c41246338fbaab9cfed3
ostertag <ostertag@e3ca9d6f-f84a-410d-b97a-e7ddc331358d>
2009-03-30 23:47:18 +0000

    adapt move of /usr/share to new perllib location
    git-svn-id: https://gpsdrive.svn.sourceforge.net/svnroot/gpsdrive/trunk@2378 e3ca9d6f-f84a-410d-b97a-e7ddc331358d

--- debian/move_utils.sh
+++ debian/move_utils.sh
@@ -7,17 +7,24 @@ destdir=debian/gpsdrive
 destdir_utils=debian/gpsdrive-utils
 mkdir -p $destdir_utils
 
-for file in `find ${destdir}/usr/local/ -type f` ; do
+# Move Perl Libs to Utils Package
+for file in `find ${destdir}/usr/share/perl5 -type f` ; do
     dst_name="${destdir_utils}/${file#${destdir}}"
     dir=`dirname $dst_name`
     mkdir -p $dir
     mv -f "${file}" "${dst_name}"
 done
+for dir in `find ${destdir}/usr/share/perl5 -type d | sort -r ` ; do
+    rmdir $dir
+done
 
 mkdir -p "${destdir_utils}/usr/"
 mkdir -p ${destdir_utils}/usr/bin
+mv -f ${destdir}/usr/bin/*.sh ${destdir_utils}/usr/bin
 mv -f ${destdir}/usr/bin/*.pl ${destdir_utils}/usr/bin
-mv -f ${destdir}/usr/bin/*.py ${destdir_utils}/usr/bin
+mv -f ${destdir}/usr/bin/*.py* ${destdir_utils}/usr/bin
+mv -f ${destdir}/usr/bin/geo* ${destdir_utils}/usr/bin
+mv -f ${destdir}/usr/bin/gpsreplay ${destdir_utils}/usr/bin
 
 mkdir -p ${destdir_utils}/usr/share/man/man1/
 for file in ${destdir_utils}/usr/bin/*; do
