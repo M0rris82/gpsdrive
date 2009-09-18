ef10075efad8c9e74fd274e475cd2ba9a1f78650
hbowman <hbowman@e3ca9d6f-f84a-410d-b97a-e7ddc331358d>
2009-09-18 07:52:45 +0000

    use real command line parser, flesh it out a bit
    git-svn-id: https://gpsdrive.svn.sourceforge.net/svnroot/gpsdrive/trunk@2511 e3ca9d6f-f84a-410d-b97a-e7ddc331358d

--- scripts/gpsdrive_tile_crush.sh
+++ scripts/gpsdrive_tile_crush.sh
@@ -17,14 +17,18 @@
 ############################################################################
 
 
-if [ $# -lt 1 ] ; then
-  echo "Usage:  $0 . [-a] [-j] [-c] [-l] [map_path]"
-  echo "use $0 --help for detailed Information"
-  exit 1
-fi
 
-if echo "$@" | grep -q -e "--help" ; then
-  echo "Usage:  gpstile_crush.sh [-a] [-j] [-c] [-l] [map_path]"
+print_usage()
+{
+  echo
+  echo "USAGE: $0 . [-a] [-j] [-c] [-n] [paths=\"map_path1:map_path2:...\"]"
+  echo "       use $0 --help for detailed Information"
+  echo
+}
+
+print_help()
+{
+  print_usage
 cat << EOF
 
  (--- Work in progress ---)
@@ -38,51 +42,154 @@ cat << EOF
  a single subdirectory. The -c flag cleans up after you removing
  all the empty .crushed tag files which are kept so that if you
  run this script again you do not have to waste time recrushing
- a tile which has already been processed. The -l flag makes it
- run in the background at low priority.
+ a tile which has already been processed. The -n flag makes it
+ run at normal priority (niceness), otherwise it tries to go low.
  
  Initially it just tries OpenStreetMap tiles, which are particularly
  bloated (saves them cycles on the server).
  
  You may wish to make backups before running this, and go out for
- a beer while it is running.
+ a beer while it is running. It may take some time.
 
 EOF
-  exit 1
+}
+
+
+# CLI parser from OSM setup-chroot script
+all_subdirs=false
+do_jpeg=false
+do_cleanup=false
+low_priority=true
+proc_path=""
+verbose=false
+#unused
+debug=false
+quiet=false
+force=false
+
+
+for arg in "$@" ; do
+   arg_true=true
+   arg_false=false
+   case $arg in
+	-a | --all)
+	    all_subdirs=$arg_true
+	    ;;
+	-j | --jpeg)
+	    do_jpeg=$arg_true
+	    ;;
+	-c | --cleanup)
+	    do_cleanup=$arg_true
+	    ;;
+	-n | --normal-priority)
+	    low_priority=$arg_false
+	    ;;
+       --paths=*) # Specify specific path to process
+	    proc_path=${arg#*=}
+	    ;;
+	-h | --help | -help)
+	    help=$arg_true
+	    ;;
+
+### the rest of these are unused but harmless and maybe useful one day
+	--force) #	force some actions
+	    force=$arg_true
+	    ;;
+
+	--v | --verbose) #	switch on verbose output
+	    verbose=$arg_true
+	    quiet=$arg_false
+	    ;;
+
+	--debug) #	switch on debugging
+	    debug=$arg_true
+	    verbose=$arg_true
+	    quiet=$arg_false
+	    ;;
+
+	--q | --quiet) #	switch on quiet Mode
+	    debug=$arg_false
+	    verbose=$arg_false
+	    quiet=$arg_true
+	    ;;
+
+	-debug)
+	    debug=$arg_true
+	    verbose=$arg_true
+	    quiet=""
+	    ;;
+	--nv) #		be a little bit less verbose
+	    verbose=''
+	    ;;
+	*)
+	    echo ""
+	    echo "Unknown option $arg"
+	    echo ""
+	    help=true
+	    UNKNOWN_OPTION=true
+	    ;;
+    esac
+done # END OF OPTIONS
+
+
+if [ "$help" = "true" ] ; then
+  print_help
+  if [ -n "$UNKNOWN_OPTION" ] ; then
+     exit 1
+  else
+     exit 0
+  fi
 fi
 
-# if [ -l flag ] ; then
+
+if [ "$low_priority" = "true" ] ; then
    renice +18 -p $$ 
-#fi
-
-
-#if [ ! -j flag ]
-if [ ! -x "`which pngcrush`" ] ; then
-   echo "ERROR: pngcrush not available. Please install it first"
-   exit 1
 fi
-#else jpeg mode
-# if [ ! -x `which pngtopnm` ] ; then
-#   echo "ERROR: NetPBM tools not available. Please install them first"
-#   exit 1
-# fi
-#fi
 
-
-if [ ! -d ~/.gpsdrive/maps/ ] ; then
-   echo "Map repository not found. Nothing to do."
-   exit 1
+if [ "$do_jpeg" = "false" ] ; then
+   if [ ! -x "`which pngcrush`" ] ; then
+      echo "ERROR: pngcrush not available. Please install it first"
+      exit 1
+   fi
+else
+   if [ ! -x `which pngtopnm` ] ; then
+      echo "ERROR: NetPBM tools not available. Please install them first"
+      exit 1
+   fi
 fi
 
 
+if [ "$verbose" = "true" ] ; then
+   quiet_opt=""
+else
+   quiet_opt="-q"
+fi
 
-cd ~/.gpsdrive/maps/
-PROCDIRS="openstreetmap_tah mapnik mapnik_cache"
 
-# TODO : command line specification of dir to process
-#if [ -d "$2" ] ; then 
-#   PROCDIRS="$2"
-#fi
+if [ -z "$proc_path" ] ; then
+   proc_path=~/.gpsdrive/maps/
+   PROCDIRS="openstreetmap_tah mapnik mapnik_cache"
+
+   if [ ! -d "$proc_path" ] ; then
+      echo "Map repository not found at <$proc_path>. Nothing to do."
+      exit 1
+   fi
+else
+   #todo: make safe for spaces in path name
+   echo "multi path= is still todo"
+   PROCDIRS=`echo "$proc_path" | tr ':' ' '`
+   # todo
+   # for DIR in $PROCDIRS ; do 
+   #   if [ ! -d "$proc_path" ] ; then
+   #      echo "Map repository not found. Nothing to do."
+   #      exit 1
+   #   fi
+   # done
+fi
+
+
+cd "$proc_path"
+
 
 for OSMER in $PROCDIRS ; do
    if [ ! -d "$OSMER" ] ; then
@@ -102,19 +209,43 @@ for OSMER in $PROCDIRS ; do
       fi
       echo "Crushing --|$MAP|--  ($i/$TOTALTILES) ..."
 
-      pngcrush -brute -reduce -c 2 -q "$MAP" "$MAP.crush"
+      if [ "$do_jpeg" = "true" ] ; then
+          echo "JPEG is TODO. Haven't touched <$MAP>"
 
-      if [ $? -eq 0 ] && [ -s "$MAP.crush" ] ; then
-         BEFORE=`wc --bytes < "$MAP"`
-         AFTER=`wc --bytes < "$MAP.crush"`
-         \mv "$MAP.crush" "$MAP"
-         touch "$MAP.crushed"  # so we don't repeat
-         echo "$BEFORE $AFTER" | \
-	    awk '{printf("   reduced %.1f%%\n", ($1-$2)*100.0/$1)}'
-          # for JPEG we'll need to update map_koord.txt too
+	  continue
+
+	  JPEG_MAP=`basename $MAP .png`.jpg
+
+	  echo "pngtopnm \"$MAP\" | pnmtojpeg --quality=85 > $JPEG_MAP"
+	  if [ $? -eq 0 ] && [ -s "$JPEG_MAP" ] ; then
+            BEFORE=`wc --bytes < "$MAP"`
+            AFTER=`wc --bytes < "$JPEG_MAP"`
+            echo "$BEFORE $AFTER" | \
+	       awk '{printf("   reduced %.1f%%\n", ($1-$2)*100.0/$1)}'
+
+	    echo \rm "$MAP"
+	    # for JPEG we'll need to update map_koord.txt too
+	    # FIXME
+	    echo sed -i -e "s/\($MAP\)/\1.jpg/" ~/.gpsdrive/maps/map_koord.txt
+
+         else
+            echo "Error crushing <$MAP>."
+            \rm "$JPEG_MAP"
+         fi
       else
-         echo "Error crushing [$MAP]."
-         \rm "$MAP.crush"
+         pngcrush -brute -reduce -c 2 $quiet_opt "$MAP" "$MAP.crush"
+
+         if [ $? -eq 0 ] && [ -s "$MAP.crush" ] ; then
+            BEFORE=`wc --bytes < "$MAP"`
+            AFTER=`wc --bytes < "$MAP.crush"`
+            \mv "$MAP.crush" "$MAP"
+            touch "$MAP.crushed"  # so we don't repeat
+            echo "$BEFORE $AFTER" | \
+	       awk '{printf("   reduced %.1f%%\n", ($1-$2)*100.0/$1)}'
+         else
+            echo "Error crushing <$MAP>."
+            \rm "$MAP.crush"
+         fi
       fi
    done
    cd ..
@@ -122,7 +253,7 @@ for OSMER in $PROCDIRS ; do
 done
 
 
-if [ "$TODO_flag" = "-c" ] ; then
+if [ "$do_cleanup" = "true" ] ; then
   for OSMER in $PROCDIRS ; do
     if [ ! -d "$OSMER" ] ; then
        continue
