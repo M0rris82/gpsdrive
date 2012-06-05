677f712cdfc4e4c19abae65c2fc9f878384b71c4
hbowman <hbowman@e3ca9d6f-f84a-410d-b97a-e7ddc331358d>
2012-06-05 08:13:46 +0000

    quote variables; minor whitespace
    git-svn-id: https://gpsdrive.svn.sourceforge.net/svnroot/gpsdrive/trunk@2652 e3ca9d6f-f84a-410d-b97a-e7ddc331358d

--- debian/create_missing_man_pages.sh
+++ debian/create_missing_man_pages.sh
@@ -1,87 +1,93 @@
 #!/bin/sh
+#
+# Traverses through GpsDrive's scripts and generates man pages where it can.
+#
 
-src_dir=$1
+src_dir="$1"
 shift
-man1_path=$1
+man1_path="$1"
 shift
 
 if echo "$@" | grep -q -e "--help" ; then
-    echo "This script tries to create Man Pages from $src_dir to $man1_path"
-    exit -1 
+    echo "This script tries to create Man Pages from [$src_dir] to [$man1_path]"
+    exit -1
 fi
 
-echo "Creating Man Pages from $src_dir to $man1_path"
+echo "Creating Man Pages from [$src_dir] to [$man1_path]"
 
 mkdir -p "$man1_path"
 
+
 # Perl Binaries
-find $src_dir -name "*.pl" | grep -v -e '\#' -e '~' |\
+find "$src_dir" -name "*.pl" | grep -v -e '\#' -e '~' |\
     while read src_fn ; do 
     filename="`basename $src_fn`"
     filename=${filename%.pl}
 
     # extract manpage from perl program
     man1_fn="$man1_path/${filename}.1"
-    if perldoc "$src_fn" >/dev/null 2>&1 ; then
+    if perldoc "$src_fn" > /dev/null 2>&1 ; then
 	echo "Create Man Page '$man1_fn' from pod '$src_fn'"
-	pod2man $src_fn >"$man1_fn"
+	pod2man $src_fn > "$man1_fn"
     else
-	if grep -q -e "--man" "$src_fn"; then
+	if grep -q -e "--man" "$src_fn" ; then
 	    echo "Create Man Page '$man1_fn' with '$src_fn'  --man"
-	    perl $src_fn --man >"$man1_fn"
+	    perl "$src_fn" --man > "$man1_fn"
 	else
-	    if grep -q -e "--help" "$src_fn"; then
+	    if grep -q -e "--help" "$src_fn" ; then
 		echo "Create Man Page '$man1_fn' with '$src_fn'  --help"
-		perl $src_fn --help >"$man1_fn"
+		perl $src_fn --help > "$man1_fn"
 	    else
-		echo "No idea how to create Man Page for $src_fn"
+		echo "No idea how to create Man Page for '$src_fn'"
 	    fi
 	fi
     fi
 done
 
+
 # Shell Scripts
-find $src_dir -name "*.sh" | grep -v -e '\#' -e '~' |\
+find "$src_dir" -name "*.sh" | grep -v -e '\#' -e '~' |\
     while read src_fn ; do 
     filename="`basename $src_fn`"
     filename=${filename%.sh}
 
     # extract manpage from perl program
     man1_fn="$man1_path/${filename}.1"
-    if grep -q -e "--man" "$src_fn"; then
+    if grep -q -e "--man" "$src_fn" ; then
 	echo "Create Man Page '$man1_fn' with '$src_fn'  --man"
-	perl $src_fn --man >"$man1_fn"
+	perl "$src_fn" --man > "$man1_fn"
     else
-	if grep -q -e "--help" "$src_fn"; then
+	if grep -q -e "--help" "$src_fn" ; then
 	    echo "Create Man Page '$man1_fn' with '$src_fn'  --help"
-	    perl $src_fn --help >"$man1_fn"
+	    perl "$src_fn" --help > "$man1_fn"
 	else
-	    echo "No idea how to create Man Page for $src_fn"
+	    echo "No idea how to create Man Page for '$src_fn'"
 	fi
     fi
 done
 
-# Python Binaries
-find $src_dir -name "*.py" | grep -v -e '\#' -e '~' |\
+
+# Python Scripts
+find "$src_dir" -name "*.py" | grep -v -e '\#' -e '~' |\
     while read src_fn ; do 
     filename="`basename $src_fn`"
     filename=${filename%.py}
 
     # extract manpage from python program
     man1_fn="$man1_path/${filename}.1"
-    if pydoc "$src_fn" >/dev/null 2>&1 ; then
+    if pydoc "$src_fn" > /dev/null 2>&1 ; then
 	echo "Create Man Page '$man1_fn' from python-file '$src_fn'"
-	pydoc $src_fn >"$man1_fn"
+	pydoc "$src_fn" > "$man1_fn"
     else
-	if grep -q -e "--man" "$src_fn"; then
+	if grep -q -e "--man" "$src_fn" ; then
 	    echo "Create Man Page '$man1_fn' with '$src_fn'  --man"
-	    python $src_fn --man >"$man1_fn"
+	    python $src_fn --man > "$man1_fn"
 	else
-	    if grep -q -e "--help" "$src_fn"; then
+	    if grep -q -e "--help" "$src_fn" ; then
 		echo "Create Man Page '$man1_fn' with '$src_fn'  --help"
-		python $src_fn --help >"$man1_fn"
+		python $src_fn --help > "$man1_fn"
 	    else
-		echo "No idea how to create Man Page for $src_fn"
+		echo "No idea how to create Man Page for '$src_fn'"
 	    fi
 	fi
     fi
